b37753651d23ab71d734e6bed4790304a644ade6
Tristan BODA <115488503+Kron0l@users.noreply.github.com>
2023-10-12 17:57:14 +0200

    Update AppConfig.js

--- public/AppConfig.js
+++ public/AppConfig.js
@@ -1,5 +1,5 @@
 AppConfig = {
-    "sceneUUID": "8175bae2-96a6-464a-997c-f56f5efcd62c"
+    "sceneUUID": "806934b2-7050-49f0-b259-e8078e2b82d7"
 }
 // const connectionInfo = await SDK3DVerse.webAPI.createSession("35d28f56-e34c-4f65-b37d-5baf31381096");
 // SDK3DVerse.setupDisplay(document.getElementById('display_canvas'));

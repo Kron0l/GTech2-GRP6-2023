e4834c4d28dbf7b6b61fce85eb828c2baefbd002
Tristan BODA <115488503+Kron0l@users.noreply.github.com>
2023-10-13 13:21:34 +0200

    Update app.js

--- public/app.js
+++ public/app.js
@@ -9,4 +9,29 @@ window.addEventListener('load', async () =>
     SDK3DVerse.setupDisplay(document.getElementById('display_canvas'));
     SDK3DVerse.startStreamer(connectionInfo);
     SDK3DVerse.connectToEditor();
+
+    async function SetResolution(canvas)
+{
+    const canvasSize    = {width: window.innerWidth, height: window.innerHeight};
+
+    const largestDim    = Math.max(canvasSize.width, canvasSize.height);
+    const MAX_DIM       = 1920;
+    const scale         = (largestDim > MAX_DIM) ? (MAX_DIM / largestDim) : 1;
+
+    let w               = Math.floor(canvasSize.width);
+    let h               = Math.floor(canvasSize.height);
+    const aspectRatio   = w/h;
+
+    if(w > h)
+    {
+        // landscape
+        w = Math.floor(aspectRatio * h);
+    }
+    else
+    {
+        // portrait
+        h = Math.floor(w / aspectRatio);
+    }
+    SDK3DVerse.setResolution(w, h, scale);
+}
 });
